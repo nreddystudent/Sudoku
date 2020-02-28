0d120fb2df8629b0124981cf3a2f9e5f64665755
Frederick Moor <fwmoor@outlook.com>
2020-02-28 09:57:01 +0200

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,11 @@
 var startBoard;
 var helpBoard;
 var help;
-var hintCount;
 let hint;
 
 function getUserBoard() {
 	let retBoard = [];
-	var c;
+	let c;
 	for (let i = 0; i < 9; i++) {
 		let temp = [];
 		for (let j = 0; j < 9; j++) {
@@ -24,20 +23,16 @@ function checkInput(num) {
 		var i = Math.floor(num / 9);
 		var j = num - (i * 9);
 		$("#cell-" + num).css('color', (!help) ? 'black' : (helpBoard[i][j] != c && help) ? 'red' : 'green');
-		if (compareBoards(getUserBoard(), helpBoard))
-			$('input').prop('disabled', false).val('').css('color', 'black').blur();
+		if (compareBoards(getUserBoard(), helpBoard)) $('input').prop('disabled', false).val('').css('color', 'black').blur();
 		return (helpBoard[i][j] != c) ? false : true;
 	}
 }
 
 function compareBoards() {
 	var userBoard = getUserBoard();
-	for (let i = 0; i < 9; i++) {
-		for (let j = 0; j < 9; j++) {
-			if (userBoard[i][j] != helpBoard[i][j])
-				return false;
-		}
-	}
+	for (let i = 0; i < 9; i++)
+		for (let j = 0; j < 9; j++)
+			if (userBoard[i][j] != helpBoard[i][j]) return false;
 	return true;
 }
 
@@ -45,38 +40,30 @@ function isValid(board, row, col, k) {
 	for (let i = 0; i < 9; i++) {
 		const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
 		const n = 3 * Math.floor(col / 3) + i % 3;
-		if (board[row][i] == k || board[i][col] == k || board[m][n] == k)
-			return false;
+		if (board[row][i] == k || board[i][col] == k || board[m][n] == k) return false;
 	}
 	return true;
 }
 
 function sudokuSolver(data) {
-	for (let i = 0; i < 9; i++) {
-		for (let j = 0; j < 9; j++) {
+	for (let i = 0; i < 9; i++)
+		for (let j = 0; j < 9; j++)
 			if (data[i][j] == 0) {
-				for (let k = 1; k <= 9; k++) {
+				for (let k = 1; k <= 9; k++)
 					if (isValid(data, i, j, k)) {
 						data[i][j] = k;
-						if (sudokuSolver(data))
-							return true;
-						else
-							data[i][j] = 0;
+						if (sudokuSolver(data)) return true;
+						else data[i][j] = 0;
 					}
-				}
 				return false;
 			}
-		}
-	}
 	return true;
 }
 
 $(document).ready(function() {
-	$("#help-group").prop('disabled', true).hide();
-	$("#getHint").prop('disabled', true).hide();
-	$("#checkBoard").prop('disabled', true).hide();
-	$("#solveBoard").prop('disabled', true).hide();
-	$("#clearBoard").prop('disabled', true).hide();
+	$("#getHint, #help-group, #checkBoard, #solveBoard, #clearBoard").prop('disabled', true).hide();
+	for (let i = 0; i < 81; i++)
+		$("#cell-" + i).on("input", () => checkInput(i)).attr("maxlength", "1");
 });
 
 $('#help').change(function () {
@@ -89,30 +76,21 @@ $('#help').change(function () {
 
 $("#newBoard").click(function(){
 	var dif = $("#dif").val();
-	hintCount = (dif == 'easy') ? 5 : (dif == 'medium') ? 3 : 1;
-	hint = hintCount;
+	hint = (dif == 'easy') ? 5 : (dif == 'medium') ? 3 : 1;
 	$.ajax(
 		{
 			url: "https://sugoku2.herokuapp.com/board?difficulty=" + dif, 
 			success: function(result)
 			{
 				$('input').prop('disabled', false).val('').css('color', 'black');
-				$("#help-group").prop('disabled', false).show();
-				$("#getHint").prop('disabled', false).show();
-				$("#checkBoard").prop('disabled', false).show();
-				$("#solveBoard").prop('disabled', false).show();
-				$("#clearBoard").prop('disabled', false).show();
+				$("#getHint, #help-group, #checkBoard, #solveBoard, #clearBoard, #help").prop('disabled', false).show();
 				$("#getHint").text(`Hint (${hint})`);
 				startBoard = result['board'];
 				helpBoard = startBoard.map(v => [...v]);
 				sudokuSolver(helpBoard);
-				for (let i = 0; i < 9; i++) {
-					for (let j = 0; j < 9; j++) {
-						if (startBoard[i][j] != 0) {
-							$("#cell-" + ((i * 9) + j)).prop('disabled', true).val(startBoard[i][j]);
-						}
-					}
-				}
+				for (let i = 0; i < 9; i++)
+					for (let j = 0; j < 9; j++)
+						if (startBoard[i][j] != 0) $("#cell-" + ((i * 9) + j)).prop('disabled', true).val(startBoard[i][j]);
 			}
 		}
 	);
@@ -121,34 +99,27 @@ $("#newBoard").click(function(){
 $("#checkBoard").click(function(){
 	if (startBoard) {
 		var userBoard = getUserBoard();
-		for (let i = 0; i < 9; i++) {
+		for (let i = 0; i < 9; i++)
 			for (let j = 0; j < 9; j++) {
-				if (startBoard[i][j] == 0 && userBoard[i][j] != '')
-					$("#cell-" + ((i * 9) + j)).css('color', (helpBoard[i][j] != userBoard[i][j]) ? 'red' : 'green');
-				else
-					$("#cell-" + ((i * 9) + j)).css('color', 'black');
+				if (startBoard[i][j] == 0 && userBoard[i][j] != '') $("#cell-" + ((i * 9) + j)).css('color', (helpBoard[i][j] != userBoard[i][j]) ? 'red' : 'green');
+				else $("#cell-" + ((i * 9) + j)).css('color', 'black');
 			}
-		}
 	}
 });
 
 $("#getHint").click(function(){
 	if (startBoard) {
 		if (!compareBoards()) {
-			var userBoard = getUserBoard();
 			let ran = Math.floor(Math.random() * 81);
-			while ($("#cell-" + ran).val()) {
+			while ($("#cell-" + ran).val())
 				ran = Math.floor(Math.random() * 81);
-			} 
 			var i = Math.floor(ran / 9);
 			var j = ran - (i * 9);
-			$("#cell-" + ran).prop('disabled', true).val(helpBoard[i][j]);
-			if (--hint == 0)
-				$("#getHint").prop('disabled', true);
+			startBoard[i][j] = helpBoard[i][j];
+			$("#cell-" + ran).prop('disabled', true).val(startBoard[i][j]);
+			if (--hint == 0) $("#getHint").prop('disabled', true);
 			$("#getHint").text(`Hint (${hint})`);
-		} else {
-			$("#getHint").prop('disabled', true);
-		}
+		} else $("#getHint").prop('disabled', true);
 	}
 })
 
@@ -156,31 +127,22 @@ $("#clearBoard").click(function(){
 	if (startBoard) {
 		for (let i = 0; i < 9; i++) {
 			for (let j = 0; j < 9; j++) {
-				if (startBoard[i][j] != 0)
-					$("#cell-" + ((i * 9) + j)).prop('disabled', true).val(startBoard[i][j]);
-				else
-					$("#cell-" + ((i * 9) + j)).prop('disabled', false).val('');
+				if (startBoard[i][j] != 0) $("#cell-" + ((i * 9) + j)).prop('disabled', true).val(startBoard[i][j]);
+				else $("#cell-" + ((i * 9) + j)).prop('disabled', false).val('');
 			}
-		}
+		};
 		$('input').css('color', 'black');
-		hint = hintCount;
-		$("#getHint").prop('disabled', false).text(`Hint (${hint})`);
-		$("#checkBoard").prop('disabled', false);
-		$("#solveBoard").prop('disabled', false);
+		$("#checkBoard, #solveBoard").prop('disabled', false);
 	}
 });
 
 $("#solveBoard").click(function(){
 	if (startBoard) {
 		$('input').css('color', 'black');
-		for (let i = 0; i < 9; i++) {
-			for (let j = 0; j < 9; j++) {
+		for (let i = 0; i < 9; i++)
+			for (let j = 0; j < 9; j++)
 				$("#cell-" + ((i * 9) + j)).prop('disabled', true).val(helpBoard[i][j]);
-			}
-		}
-		$("#getHint").prop('disabled', true);
-		$("#checkBoard").prop('disabled', true);
-		$("#solveBoard").prop('disabled', true);
+		$("#getHint, #checkBoard, #solveBoard, #clearBoard, #help").prop('disabled', true);
 	}
 });
 
