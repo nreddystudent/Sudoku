78866d2b064d61a9695b594a0e4d683747dcb49c
Frederick Moor <fwmoor@outlook.com>
2020-02-28 09:57:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-	// Create the browser window.
 	const win = new BrowserWindow({
 		width: 700,
 		height: 700,
 		webPreferences: {
-		nodeIntegration: true
+			nodeIntegration: true
 		}
 	})
-
-	// and load the index.html of the app.
 	win.loadFile('index.html')
 	win.setMenu(null)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
