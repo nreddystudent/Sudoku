a17479f147df91febd0d6a10aab4bfe6dc3ee8b3
fwmoor <fwmoor@outlook.com>
2021-09-04 14:45:24 +0200

    fix: console warning removed

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
+function createWindow() {
 	const win = new BrowserWindow({
 		width: 700,
 		height: 700,
 		webPreferences: {
 			nodeIntegration: true
-		}
+		},
+		allowRendererProcessReuse: true
 	})
 	win.loadFile('index.html')
 	win.setMenu(null)
 }
+
+app.allowRendererProcessReuse = true;
 
 app.whenReady().then(createWindow)
